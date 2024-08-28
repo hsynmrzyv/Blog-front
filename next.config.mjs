6ea463472f7b9a3d376b9e67e5c3f893b2093393
Huseyn Mirzayev <huseynmirzayev8@gmail.com>
2024-08-28 15:08:04 +0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
+        protocol: "https",
+        hostname: "blogpf201-api.onrender.com",
       },
     ],
   },
